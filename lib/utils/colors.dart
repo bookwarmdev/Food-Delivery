86db972480d6b26036edcbc23a23b7bf62d85c9f
Amoo .O. Faruk <bookwarmdev@gmail.com>
2021-12-17 09:13:08 +0100

    project file sturture

---
+++ lib/utils/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0x00fead1d);
---
+++ lib/utils/fonts.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kTextHeader = TextStyle(
+  fontSize: 31.0,
+);
