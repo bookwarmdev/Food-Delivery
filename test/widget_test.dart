de0381dd9bdce63ab1cd1dfe655d2242c82cdda3
Amoo .O. Faruk <bookwarmdev@gmail.com>
2022-05-18 14:49:11 +0100

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,9 +5,29 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
- 
+import 'package:food_delivery/views/onbording/onbording.dart';
+
 void main() {
-  testWidgets(
-      'Mywidget as a title and message', (WidgetTester tester) async {});
+  testWidgets('check next screen', (WidgetTester tester) async {
+    // find all widget needed
+    final nextOnbording1 = find.byKey(const ValueKey('onbording1'));
+     
+
+    // execute the actual test
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Onbording2Screen(),
+      ),
+    );
+    await tester.tap(nextOnbording1);
+    await tester.pump();
+
+    // check the output
+    // expect(
+    //   find.byType(),
+    //   findsOneWidget,
+    // );
+  });
 }
